f4bf3bdccae735420871eeadf4c25f1fc2e79185
Fatih Kaan Salgır <31157309+fatihkaan22@users.noreply.github.com>
2020-06-01 02:11:28 +0300

    description

--- upload-qr.sh
+++ upload-qr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Uploads file to file.io and shows qr code to share
+
 key=$(curl -F "file=@$1" https://file.io | cut -d ',' -f 2 |  cut -d '"' -f 4)
 link="https://file.io/$key"
 echo $link
