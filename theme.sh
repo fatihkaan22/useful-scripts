4459476d9e1ad9e6f743198d7b6f1ec8147144ec
fatihkaan22 <fatihkaan22@gmail.com>
2021-05-16 19:37:46 +0300

    generic functions, more apps

--- theme.sh
+++ theme.sh
@@ -1,39 +1,63 @@
 # The theme script to rule them all!
 
-# gtk theme
-function xsettings() {
+function commentLine() {
+	local match=$1
+	local how=$2
+	local file=$3
+	sed -i "s|.*${match}|${how} ${match}|" ${file}
+}
+
+function uncommentLine() {
+	local match=$1
+	local how=$2
+	local file=$3
+	sed -i "s|.*${how} ${match}|${match}|" ${file}
+}
+
+function switchTo() {
+	local darkconf=$2
+	local lightconf=$3
+	local how=$4
+	local file=$5
 	if [[ $1 == "dark" ]]; then
-		sed -i 's/.*Net\/ThemeName "Ant"/# Net\/ThemeName "Ant"/' ~/.xsettingsd
-		sed -i 's/.*# Net\/ThemeName "Ant-Dracula-Blue"/Net\/ThemeName "Ant-Dracula-Blue"/' ~/.xsettingsd 
+		commentLine "$lightconf" "$how" $file
+		uncommentLine "$darkconf" "$how" $file
 	fi
 	if [[ $1 == "light" ]]; then
-		sed -i 's/.*# Net\/ThemeName "Ant"/Net\/ThemeName "Ant"/' ~/.xsettingsd
-		sed -i 's/.*Net\/ThemeName "Ant-Dracula-Blue"/# Net\/ThemeName "Ant-Dracula-Blue"/' ~/.xsettingsd
+		uncommentLine "$lightconf" "$how" $file
+		commentLine "$darkconf" "$how" $file
 	fi
+}
+
+# gtk theme
+function xsettings() {
+	local darkconf='Net/ThemeName "Ant-Dracula-Blue"'
+	local lightconf='Net/ThemeName "Ant"'
+	local file=~/.xsettingsd
+	switchTo $1 "$darkconf" "$lightconf" '#' $file
 }
 
 function gtk() {
-	if [[ $1 == "dark" ]]; then
-		sed -i 's/gtk-application-prefer-dark-theme=false/gtk-application-prefer-dark-theme=true/' ~/.config/gtk-3.0/settings.ini
-	fi
-	if [[ $1 == "light" ]]; then
-		sed -i 's/gtk-application-prefer-dark-theme=true/gtk-application-prefer-dark-theme=false/' ~/.config/gtk-3.0/settings.ini
-	fi
+	local darkconf='gtk-application-prefer-dark-theme=true'
+	local lightconf='gtk-application-prefer-dark-theme=false'
+	local file=~/.config/gtk-3.0/settings.ini
+	switchTo $1 "$darkconf" "$lightconf" '#' $file
 }
 
-function telegram() {
-	pid=$(pgrep telegram-deskto)
-	echo $pid
-	if [[ -n "$pid" ]]; then
-		kill $pid
-		telegram-desktop &
-	fi
-}
+# restart telegram
+# function telegram() {
+# 	pid=$(pgrep telegram-deskto)
+# 	echo $pid
+# 	if [[ -n "$pid" ]]; then
+# 		kill $pid
+# 		telegram-desktop &
+# 	fi
+# }
 
 # kitty needs to be run with --single-instance option
 function kittytheme() {
 	if [[ $1 == "dark" ]]; then
-		kitty @ set-colors -a -c /home/near/.config/kitty/theme.conf
+		kitty @ set-colors -a -c /home/near/.config/kitty/dracula.conf
 	fi
 	if [[ $1 == "light" ]]; then
 		kitty @ set-colors -a -c /home/near/.config/kitty/kitty-themes/themes/AtomOneLight.conf
@@ -42,7 +66,6 @@ function kittytheme() {
 
 # nvr: https://github.com/mhinz/neovim-remote
 function vimtheme() {
-	vthm=""
 	if [[ $1 == "dark" ]]; then
 		vthm="set bg=dark"
 	fi
@@ -53,6 +76,31 @@ function vimtheme() {
 		nvr --servername $i -c "$vthm" &
 	done
 }
+
+function vimrc() {
+	local darkconf='color dracula'
+	local lightconf='color one'
+	local file=~/.vimrc
+	switchTo $1 "$darkconf" "$lightconf" '"' $file
+	local darkconf='set bg=dark'
+	local lightconf='set bg=light'
+	switchTo $1 "$darkconf" "$lightconf" '"' $file
+}
+
+function batconfig() {
+	local darkconf='--theme="Dracula"'
+	local lightconf='--theme="OneHalfLight"'
+	local file=~/.config/bat/config
+	switchTo $1 "$darkconf" "$lightconf" '#' $file
+}
+
+function kittyconf() {
+	local darkconf="include ./dracula.conf"
+	local lightconf="include ./kitty-themes/themes/Material.conf"
+	local file=~/.config/kitty/kitty.conf
+	switchTo $1 "$darkconf" "$lightconf" '#' $file
+}
+
 
 if [[ -n "$1"  &&  $1 == "dark" || $1 == "light" ]];then
   mode="$1"
@@ -65,9 +113,9 @@ xsettings $mode
 gtk $mode
 kittytheme $mode
 vimtheme $mode
-
-# reload
-# telegram
+vimrc $mode
+batconfig $mode
+kittyconf $mode
 
 # restart daemon
 xsettingsd > /dev/null 2>&1 &
